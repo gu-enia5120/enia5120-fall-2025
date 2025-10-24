f20825fa981c0e899fc535f11332d61f7dca794c
Abhijit Dasgupta <aikidasgupta@gmail.com>
2025-10-24 01:28:35 -0400

    updated website on week7

--- scripts/_canvas.R
+++ scripts/_canvas.R
@@ -1,6 +1,7 @@
 library(vvcanvas)
 
-api_key = grep('GU_CANVAS', readLines('~/.creds'), value = T) |> str_extract("(?<=\").*?(?=\")")
+api_key = grep('GU_CANVAS', readLines('~/.creds'), value = T) |>
+  str_extract("(?<=\").*?(?=\")")
 base_url <- "https://georgetown.instructure.com"
 canvas <- canvas_authenticate(api_key, base_url)
 enia5120 <- get_courses(canvas) |> filter(str_detect(course_code, "ENIA"))
--- scripts/_create_gdrive_files.R
+++ scripts/_create_gdrive_files.R
@@ -2,7 +2,7 @@ library(googledrive)
 library(tidyverse) |> suppressMessages()
 drive_auth(email = "ad1704@georgetown.edu")
 
-for (i in 3:14){
+for (i in 3:14) {
   drive_create(paste('Week', i), type = 'document', path = "ENIA 5120")
 }
 
@@ -11,4 +11,3 @@ notes <- drive_ls("ENIA 5120", type = 'document')
 bl = drive_share(notes$id, role = 'writer', type = 'anyone') |>
   mutate(links = drive_link(id))
 saveRDS(bl, here::here('_artifacts', 'gdrive_shared_notes.rds'))
-
